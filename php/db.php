4d5ca314bc5d76266b4ccdae99f856878d4a10e9
Blocaholic <tobias@rocklands.de>
2023-03-18 21:40:18 +0100

    Add database handling

--- php/db.php
+++ php/db.php
@@ -1,3 +1,29 @@
 <?php
+class DB {
+  static function connect() {
+    try {
+      $user = $name = 'd03c79cf';
+      $password = 'ZTsLMR9okcpMNqhM';
 
+      $pdo = new PDO("mysql:dbname=$name;host=localhost", $user, $password);
+      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      return $pdo;
+    } catch (PDOException $e) {
+      throw $e;
+    }
+  }
+
+  public static function getFavorites() {
+    try {
+      $pdo = DB::connect();
+      $query = 'SELECT * FROM favorites;';
+      $statement = $pdo->prepare($query);
+      $statement->execute();
+      $rows = $statement->fetchAll(PDO::FETCH_ASSOC);
+      return $rows;
+    } catch (PDOException $e) {
+      throw $e;
+    }
+  }
+}
 ?>
--- php/main.php
+++ php/main.php
@@ -2,9 +2,13 @@
 include_once 'db.php';
 
 function main() {
+  /*
   $favorites = '<div class="card"><a href="https://www.google.de/"
   target="_blank"
   rel="noopener noreferrer">Google</a></div>';
+  */
+  var_dump(DB::getFavorites());
+  $favorites = DB::getFavorites();
   $html_template = file_get_contents('../templates/index.html');
   $html = preg_replace('/\[\%favorites\%\]/', $favorites, $html_template);
   return $html;
