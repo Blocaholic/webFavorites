87c1712470b66a494114693ce40a53c09a43bd01
Blocaholic <tobias@rocklands.de>
2023-03-19 11:01:09 +0100

    Get categories from database

--- php/main.php
+++ php/main.php
@@ -2,6 +2,27 @@
 include_once 'db.php';
 
 function main() {
+  // callbacks
+  $filter_info = function (array $link): bool {
+    return $link['category'] == 'info';
+  };
+  // PRODUCTION
+  $links = DB::getLinks();
+  $categories = array_unique(array_column($links, 'category'));
+
+  // DEVELOP
+
+  $info = array_filter($links, $filter_info);
+  $html_favorite = links_to_html($links);
+  $html_info = links_to_html($info);
+
+  $html_template = file_get_contents('../templates/index.html');
+  $html = preg_replace('/\[\%favorite\%\]/', $html_favorite, $html_template);
+  $html = preg_replace('/\[\%info\%\]/', $html_info, $html);
+  return $html;
+}
+
+function links_to_html(array $links): string {
   $link_to_html = function (array $link): string {
     $html_link =
       '<a href="' .
@@ -11,15 +32,16 @@ function main() {
       '</span></div></a>';
     return $html_link;
   };
-
-  $links = DB::getLinks();
-  $html_links = array_map($link_to_html, $links);
-
-  $html_favorites = join($html_links);
-
-  $html_template = file_get_contents('../templates/index.html');
-  $html = preg_replace('/\[\%favorite\%\]/', $html_favorites, $html_template);
-  return $html;
+  return join(array_map($link_to_html, $links));
 }
-
 ?>
+
+<!--
+  [%favorite%]
+  [%info%]
+  [%finance%]
+  [%social%]
+  [%shopping%]
+  [%project%]
+  [%webdev%]
+-->
